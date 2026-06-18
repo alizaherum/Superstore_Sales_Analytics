81ee43f5eed9c5655c9c64746631cf36d6147655
Alizah Erum <alizaherum34@gmail.com>
2026-06-18 23:32:43 +0100

    Add files via upload

---
+++ stg_orders.sql
@@ -0,0 +1,23 @@
+SELECT
+    ROW_ID,
+    ORDER_ID,
+    ORDER_DATE,
+    SHIP_DATE,
+    SHIP_MODE,
+    CUSTOMER_ID,
+    CUSTOMER_NAME,
+    SEGMENT,
+    COUNTRY,
+    CITY,
+    STATE,
+    POSTAL_CODE,
+    REGION,
+    PRODUCT_ID,
+    CATEGORY,
+    SUB_CATEGORY,
+    PRODUCT_NAME,
+    SALES,
+    QUANTITY,
+    DISCOUNT,
+    PROFIT
+FROM {{ source('raw', 'ORDER_INFO') }}
